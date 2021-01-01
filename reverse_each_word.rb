16283ebb2b26f5c040b50113c51905b15bd0748a
Dayne Dewar <daynedewar@yahoo.com>
2021-01-01 06:08:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   array_2 = []
   array = string.split(" ")
   
-  array.each do |str|
+  array.collect do |str|
     array_2 << str.reverse
   end
 return array_2.join(' ')
